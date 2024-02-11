6281c958e83fa0a27e156a87ce76384cf92b3837
ZyeByteVT <102230672+zyebytevt@users.noreply.github.com>
2024-02-11 17:19:21 +0100

    Update wasmer-d

---
+++ gen-binding.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+[ -z "$GCC_INCLUDE" ] && GCC_INCLUDE=$(gcc -Wp,-v -xc /dev/null -fsyntax-only 2>&1 | grep '#include <...> search starts here:' -A1 | tail -1 | awk '{$1=$1};1')
+
+if [ ! -d "$GCC_INCLUDE" ]; then
+    >&2 echo "GCC_INCLUDE is not a directory: $GCC_INCLUDE"
+    exit 1
+fi
+
+[ -z "$WASMER_INCLUDE" ] && WASMER_INCLUDE=$(dirname $(find /usr -name wasmer.h 2>/dev/null | tail -1))
+
+dub run dpp -- --preprocess-only --no-sys-headers --ignore-macros --include-path "$GCC_INCLUDE" --include-path "$WASMER_INCLUDE" source/wasmer/bindings/wasmer.dpp
